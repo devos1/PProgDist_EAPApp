f27fc8739866ab022d7ae8dc139b4e07a5a2d70c
Oscar <oscardasilva@MacBook-Pro-de-Oscar.local>
2016-06-11 19:53:18 +0200

    Ajout la création de véhicules

--- appClientModule/InitBD.java
+++ appClientModule/InitBD.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Properties;
 import javax.naming.Context;
@@ -5,7 +7,10 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.persistence.PersistenceException;
 import entities.CategorieVehicule;
-import entities.Unite;
+import entities.EnumCatPrix;
+import entities.EnumMarque;
+import entities.EnumUnite;
+import entities.Vehicule;
 import session.*;
 
 public class InitBD {
@@ -16,15 +21,34 @@ public class InitBD {
 			Context ctx = new InitialContext(prop);
 			IMobiOsLoRemote service = (IMobiOsLoRemote) ctx.lookup("ejb:PProgDist_EAP/PProgDist_EAPEJB/MobiOsLoService!session.IMobiOsLoRemote?stateful");
 
+			// ======================================
+			// CATEGORIE DE VEHICULES            
+			// ======================================
 			// Création et insertion catégories dans la base
-			service.addCatVehicule(new CategorieVehicule("A",2.0,3.0,Unite.Heure));
-			service.addCatVehicule(new CategorieVehicule("B",3.0,14.1,Unite.Minute));
+			service.addCatVehicule(new CategorieVehicule("A",2.0,3.0,EnumUnite.Heure));
+			service.addCatVehicule(new CategorieVehicule("B",3.0,14.1,EnumUnite.Minute));
 
 			// Lister les catégories
 			List<CategorieVehicule> catVehicules = service.getCatVehicules();
 			for (CategorieVehicule cv : catVehicules) {
 				System.out.println(cv);
 			}
+			// ======================================
+			// FIN CATEGORIE DE VEHICULES            
+			// ======================================
+		
+			// ======================================
+			// VEHICULES            
+			// ======================================
+			service.addVehicule(new Vehicule(new GregorianCalendar(2014, 0, 1), "Bleu", EnumCatPrix.Budget, EnumMarque.Audi, "A1"));
+			service.addVehicule(new Vehicule(new GregorianCalendar(2010, 4, 12), "Gris", EnumCatPrix.Cabrio, EnumMarque.BMW, "Z3"));
+			service.addVehicule(new Vehicule(new GregorianCalendar(2001, 2, 14), "Noir", EnumCatPrix.Economy, EnumMarque.Dacia, "Entry"));
+			service.addVehicule(new Vehicule(new GregorianCalendar(2012, 1, 7), "Blanc", EnumCatPrix.Electro, EnumMarque.Renault, "Zoe"));
+			service.addVehicule(new Vehicule(new GregorianCalendar(2015, 8, 8), "Rouge", EnumCatPrix.Emotion, EnumMarque.BMW, "M3"));
+			// ======================================
+			// FIN VEHICULES            
+			// ======================================
+			
 		} catch (PersistenceException pe) {
 			pe.printStackTrace();
 		} catch (NamingException ne){
